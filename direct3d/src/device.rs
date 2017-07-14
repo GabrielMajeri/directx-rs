f6f272dbae424a64a0289f75f2db3dcdc747891f
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-07-14 20:59:55 +0300

    Fix D3D11CreateDevice when custom adapter is given

--- direct3d/src/device.rs
+++ direct3d/src/device.rs
@@ -14,13 +14,13 @@ pub struct Device(ComPtr<d3d11::ID3D11Device>);
 impl Device {
 	/// Creates a new device.
 	// TODO: options
-	pub fn new(_adapter: Option<Adapter>) -> Self {
+	pub fn new(adapter: Option<Adapter>) -> Self {
 		let device = ComPtr::new_with(move |ptr| {
-/*			let adapter_ptr = match adapter {
+			let adapter = match adapter {
 				None => ptr::null_mut(),
 				Some(adapter) => unsafe { mem::transmute(adapter) }
 			};
-*/
+
 			// Require at least Direct3D 11.1.
 			let feature_levels = [
 				d3dcommon::D3D_FEATURE_LEVEL_11_1,
@@ -35,11 +35,12 @@ impl Device {
 			let result = unsafe {
 				// See https://msdn.microsoft.com/en-us/library/windows/desktop/ff476082(v=vs.85).aspx for reference.
 				d3d11::D3D11CreateDevice(
-					ptr::null_mut(), //adapter_ptr,
-					d3dcommon::D3D_DRIVER_TYPE_HARDWARE,
+					adapter,
+					d3dcommon::D3D_DRIVER_TYPE_UNKNOWN,
 					// TODO: support software renderer.
 					ptr::null_mut(),
 					// See https://msdn.microsoft.com/en-us/library/windows/desktop/ff476107(v=vs.85).aspx for a list of flags.
+					// TODO: support the debug layer.
 					0,
 					feature_levels.as_ptr(),
 					feature_levels.len() as u32,
