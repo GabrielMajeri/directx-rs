cff78750e73a49d623e4bb115f46d3ea5c441ee1
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-07-15 12:05:02 +0300

    Refactor `direct2d` crate

--- direct2d/src/lib.rs
+++ direct2d/src/lib.rs
@@ -3,11 +3,11 @@
 #![cfg(windows)]
 #![deny(warnings, missing_docs)]
 
-extern crate comptr;
-use comptr::ComPtr;
+extern crate common;
+use common::ComPtr;
+use common::winapi::{d2d1, Interface};
 
-extern crate winapi;
-use winapi::um::d2d1;
+use std::ptr;
 
 /// Interface used to create other D2D objects.
 pub struct Factory(ComPtr<d2d1::ID2D1Factory>);
@@ -15,27 +15,29 @@ pub struct Factory(ComPtr<d2d1::ID2D1Factory>);
 impl Factory {
 	/// Creates a new factory.
 	pub fn new() -> Self {
-		let factory = ComPtr::new_with(|factory| {
-			use std::mem;
-
+		let factory = ComPtr::<d2d1::ID2D1Factory>::new({
 			let options = d2d1::D2D1_FACTORY_OPTIONS {
 				// TODO: support debug output.
 				debugLevel: d2d1::D2D1_DEBUG_LEVEL_NONE
 			};
 
+			let mut factory = ptr::null_mut();
+
 			let result = unsafe {
-				use winapi::Interface;
 				d2d1::D2D1CreateFactory(
 					// See https://msdn.microsoft.com/en-us/library/windows/desktop/dd368104(v=vs.85).aspx
 					// TODO: multithreading
 					d2d1::D2D1_FACTORY_TYPE_SINGLE_THREADED,
 					&d2d1::ID2D1Factory::uuidof(),
 					&options,
-					mem::transmute(factory)
+					&mut factory
 				)
 			};
 
+			// TODO: error handling.
 			assert_eq!(result, 0);
+
+			factory as *mut _
 		});
 
 		Factory(factory)
